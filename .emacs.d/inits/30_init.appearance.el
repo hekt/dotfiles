50f23176c8c22d57c8d3a22ae4946206e21a5bba
hekt <hektorg@gmail.com>
2012-09-09 22:22:41 +0900

    .emacs.d: Add shell-mode color settings

--- .emacs.d/inits/30_init.appearance.el
+++ .emacs.d/inits/30_init.appearance.el
@@ -1,3 +1,10 @@
 ;; appearance
 
 (load-theme 'solarized-dark t)
+
+;; set shell-mode colors
+(setq ansi-color-names-vector
+      ["#002B36" "#DC322F" "#859900" "#B58900"
+       "#268BD2""#D33682" "#2AA198" "#FDF6E3"]
+      )
+(setq ansi-color-map (ansi-color-make-color-map))
