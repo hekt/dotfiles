916b0a045aff4d517e8033802223279d21d22710
hekt <hektorg@gmail.com>
2012-09-10 22:09:57 +0900

    .emacs.d: Add color names as a comment

--- .emacs.d/inits/30_init.appearance.el
+++ .emacs.d/inits/30_init.appearance.el
@@ -4,7 +4,13 @@
 
 ;; set shell-mode colors
 (setq ansi-color-names-vector
-      ["#002B36" "#DC322F" "#859900" "#B58900"
-       "#268BD2""#D33682" "#2AA198" "#FDF6E3"]
-      )
+      ["#002B36" ; black
+       "#DC322F" ; red
+       "#859900" ; green
+       "#B58900" ; yellow
+       "#268BD2" ; blue
+       "#D33682" ; magenta
+       "#2AA198" ; cyan
+       "#FDF6E3" ; white
+       ])
 (setq ansi-color-map (ansi-color-make-color-map))
