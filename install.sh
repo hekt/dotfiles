a0efaf4071b2bc78c0a76711bb746b2c1b59cf78
hekt <hektorg@gmail.com>
2012-09-14 03:27:45 +0900

    install.sh: Modify progress messages

--- install.sh
+++ install.sh
@@ -12,11 +12,11 @@ mkdir $backup_dir
 cd $dotfile_dir
 
 for dotfile in .?*; do
-    if [ $dotfile != '..' ] && [ $dotfile != '.git' ]; then
+    if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.osx' ]; then
         echo "$dotfile"
         if [ -e "$home_dir/$dotfile" ]; then
-            mv "$home_dir/$dotfile" $backup_dir
             echo -e "${ylw}===>${nrm} already exists in $home_dir"
+            mv "$home_dir/$dotfile" $backup_dir
             echo -e "${ylw}===>${nrm} move to $backup_dir"
         fi
         ln -s "$dotfile_dir/$dotfile" $home_dir
@@ -25,7 +25,9 @@ for dotfile in .?*; do
 done
 
 if [ ! "$(ls -A $backup_dir )" ]; then
+    echo "$backup_dir is empty"
     rmdir $backup_dir
+    echo "remove $backup_dir"
 fi
 
 echo -e "${grn}complete${nrm}"
