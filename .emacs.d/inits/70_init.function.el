791378c35539fddc3ff7d540e32edda6b02b5074
hekt <hektorg@gmail.com>
2012-09-14 23:57:29 +0900

    .emacs.d: Fix the regexp to use lazy matching

--- .emacs.d/inits/70_init.function.el
+++ .emacs.d/inits/70_init.function.el
@@ -1,13 +1,12 @@
 ;; convert html to syntax-highlighter compatible
 (defun md-to-sh (start end)
   (interactive "r")
-  (save-excursion
-    (replace-regexp
-     "<!-- \\(.+\\) -->
+  (replace-regexp
+   "<!-- \\(.+\\) -->
 
 <pre><code>
-\\(\\(.*
-*\\)+\\)
+\\(\\(.*?
+*?\\)+?\\)
 </code></pre>"
-     "<pre class=\"brush: \\1\">\\2</pre>" 
-     nil start end)))
+   "<pre class=\"brush: \\1\">\\2</pre>" 
+   nil start end))
