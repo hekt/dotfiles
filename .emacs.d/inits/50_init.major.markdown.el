010ec17f67ddb07aa4c7a72f8ef2687f1b5c859c
hekt <hektorg@gmail.com>
2012-09-14 23:16:50 +0900

    .emacs.d: Change the Markdown conversion script

--- .emacs.d/inits/50_init.major.markdown.el
+++ .emacs.d/inits/50_init.major.markdown.el
@@ -5,10 +5,4 @@
 (add-to-list 'auto-mode-alist '("\\.mdown" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.mdwn" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(markdown-css-path "file:///Users/kaz/Dropbox/Markdown/css/markdown.css")
- '(scroll-bar-mode nil))
+(setq markdown-command "mdown")
