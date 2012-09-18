bf5519db301945ba406aa027b3afe99eb3606833
hekt <hektorg@gmail.com>
2012-09-18 14:50:09 +0900

    .emacs.d: Install ir-black-theme and modify it.

--- .emacs.d/inits/30_init.appearance.el
+++ .emacs.d/inits/30_init.appearance.el
@@ -1,16 +1,18 @@
 ;; appearance
 
-(load-theme 'solarized-dark t)
+(if window-system (load-theme 'solarized-dark t) (load-theme 'ir-black t))
 
 ;; set shell-mode colors
-(setq ansi-color-names-vector
-      ["#002B36" ; black
-       "#DC322F" ; red
-       "#859900" ; green
-       "#B58900" ; yellow
-       "#268BD2" ; blue
-       "#D33682" ; magenta
-       "#2AA198" ; cyan
-       "#FDF6E3" ; white
-       ])
-(setq ansi-color-map (ansi-color-make-color-map))
+(when window-system
+    (progn
+      (setq ansi-color-names-vector
+            ["#002B36" ; black
+             "#DC322F" ; red
+             "#859900" ; green
+             "#B58900" ; yellow
+             "#268BD2" ; blue
+             "#D33682" ; magenta
+             "#2AA198" ; cyan
+             "#FDF6E3" ; white
+             ])
+      (setq ansi-color-map (ansi-color-make-color-map))))
