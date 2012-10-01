433a61c41a33538c7890de834567c474a76d6c4f
hekt <hektorg@gmail.com>
2012-10-02 01:53:18 +0900

    .emacs.d: Modify term-mode keybinds

--- .emacs.d/inits/10_init.keybind.el
+++ .emacs.d/inits/10_init.keybind.el
@@ -3,29 +3,21 @@
 (setq mac-option-modifier 'meta)
 (define-key global-map (kbd "C-h") 'delete-backward-char)
 (define-key global-map (kbd "C-o") 'other-window)
-(define-key term-raw-map (kbd "C-h") 'delete-backward-char)
-(define-key term-raw-map (kbd "C-o") 'other-window)
 
 ;; sence-region
 (define-key global-map (kbd "C-c s") 'cua-set-rectangle-mark)
 
 ;; anything
 (define-key global-map (kbd "C-c a") 'anything)
-(define-key term-raw-map (kbd "C-c a") 'anything)
 
 ;; magit
 (define-key global-map (kbd "C-c m") 'magit-status)
-(define-key term-raw-map (kbd "C-c m") 'magit-status)
 
 ;; windmove
 (define-key global-map (kbd "C-c p") 'windmove-up)
 (define-key global-map (kbd "C-c n") 'windmove-down) 
 (define-key global-map (kbd "C-c b") 'windmove-left) 
 (define-key global-map (kbd "C-c f") 'windmove-right)
-(define-key term-raw-map (kbd "C-c p") 'windmove-up)
-(define-key term-raw-map (kbd "C-c n") 'windmove-down)
-(define-key term-raw-map (kbd "C-c b") 'windmove-left)
-(define-key term-raw-map (kbd "C-c f") 'windmove-right)
 
 ;; copy / paste / cut
 (if window-system 
@@ -43,17 +35,32 @@
       (define-key global-map (kbd "s-v") 'paste-from-pasteboard)
       (define-key global-map (kbd "s-c") 'copy-to-pasteboard)
       (define-key global-map (kbd "s-x") 'cut-to-pasteboard)
-      (define-key term-raw-map (kbd "s-v") 'paste-from-pasteboard)
-      (define-key term-raw-map (kbd "s-c") 'copy-to-pasteboard)
-      (define-key term-raw-map (kbd "s-x") 'cut-to-pasteboard)
       )
   (progn
     (require 'osx-clipboard)
     (define-key global-map (kbd "C-c M-v") 'osx-pbpaste)
     (define-key global-map (kbd "C-c M-c") 'osx-pbcopy)
     (define-key global-map (kbd "C-c M-x") 'osx-pbcut)
-    (define-key term-raw-map (kbd "C-c M-v") 'osx-pbpaste)
-    (define-key term-raw-map (kbd "C-c M-c") 'osx-pbcopy)
-    (define-key term-raw-map (kbd "C-c M-x") 'osx-pbcut)
     )
 )
+
+;; term-mode
+(add-hook 'term-mode-hook '(lambda()
+  (define-key term-raw-map (kbd "M-x") 'nil)
+  (define-key term-raw-map (kbd "C-y") 'term-paste)
+  (define-key term-raw-map (kbd "C-o") 'other-window)
+  (define-key term-raw-map (kbd "C-c a") 'anything)
+  (define-key term-raw-map (kbd "C-c m") 'magit-status)
+  (define-key term-raw-map (kbd "C-c p") 'windmove-up)
+  (define-key term-raw-map (kbd "C-c n") 'windmove-down)
+  (define-key term-raw-map (kbd "C-c b") 'windmove-left)
+  (define-key term-raw-map (kbd "C-c f") 'windmove-right)
+  (when window-system
+    (progn
+      (define-key term-raw-map (kbd "s-v") 'paste-from-pasteboard)
+      (define-key term-raw-map (kbd "s-c") 'copy-to-pasteboard)
+      (define-key term-raw-map (kbd "s-x") 'cut-to-pasteboard))
+    (progn
+      (define-key global-map (kbd "C-c M-v") 'osx-pbpaste)
+      (define-key global-map (kbd "C-c M-c") 'osx-pbcopy)
+      (define-key global-map (kbd "C-c M-x") 'osx-pbcut)))))
