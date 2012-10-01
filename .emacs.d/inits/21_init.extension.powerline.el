b76fd8bf04d1327ea5f0d6a8125d7342dfabd8d6
hekt <hektorg@gmail.com>
2012-10-02 03:59:19 +0900

    .emacs.d: Modify indent

--- .emacs.d/inits/21_init.extension.powerline.el
+++ .emacs.d/inits/21_init.extension.powerline.el
@@ -2,9 +2,9 @@
 
 (when window-system (progn
 
-(defun arrow-right-xpm (color1 color2)
-  "Return an XPM right arrow string representing."
-  (format "/* XPM */
+  (defun arrow-right-xpm (color1 color2)
+    "Return an XPM right arrow string representing."
+    (format "/* XPM */
 static char * arrow_right[] = {
 \"10 15 2 1\",
 \". c %s\",
@@ -25,9 +25,9 @@ static char * arrow_right[] = {
 \"..        \",
 \".         \"};"  color1 color2))
 
-(defun arrow-left-xpm (color1 color2)
-  "Return an XPM right arrow string representing."
-  (format "/* XPM */
+  (defun arrow-left-xpm (color1 color2)
+    "Return an XPM right arrow string representing."
+    (format "/* XPM */
 static char * arrow_right[] = {
 \"10 15 2 1\",
 \". c %s\",
@@ -49,62 +49,63 @@ static char * arrow_right[] = {
 \"         .\"};"  color2 color1))
 
 
-(defconst color1 "#859900")
-(defconst color2 "#586E75")
-(defconst color3 "#839496")
-(defconst fontcolor1 "#FDF6E3")
-(defconst fontcolor2 "#EEE8D5")
+  (defconst color1 "#859900")
+  (defconst color2 "#586E75")
+  (defconst color3 "#839496")
+  (defconst fontcolor1 "#FDF6E3")
+  (defconst fontcolor2 "#EEE8D5")
 
-(defvar arrow-right-1 (create-image (arrow-right-xpm color1 color2) 
-                                    'xpm t :ascent 'center))
-(defvar arrow-right-2 (create-image (arrow-right-xpm color2 "None") 
-                                    'xpm t :ascent 'center))
-(defvar arrow-left-1  (create-image (arrow-left-xpm color2 color3) 
-                                    'xpm t :ascent 'center))
-(defvar arrow-left-2  (create-image (arrow-left-xpm "None" color2) 
-                                    'xpm t :ascent 'center))
+  (defvar arrow-right-1 (create-image (arrow-right-xpm color1 color2) 
+                                      'xpm t :ascent 'center))
+  (defvar arrow-right-2 (create-image (arrow-right-xpm color2 "None") 
+                                      'xpm t :ascent 'center))
+  (defvar arrow-left-1  (create-image (arrow-left-xpm color2 color3) 
+                                      'xpm t :ascent 'center))
+  (defvar arrow-left-2  (create-image (arrow-left-xpm "None" color2) 
+                                      'xpm t :ascent 'center))
 
-(setq-default mode-line-format
- (list  '(:eval (concat (propertize 
-                         (concat " " (format-mode-line mode-name) " ") 
-                         'face 'mode-line-color-1)
-                        (propertize " " 'display arrow-right-1)))
-        '(:eval (concat (propertize " %* %b " 'face 'mode-line-color-2)
-                        (propertize " " 'display arrow-right-2)))
+  (setq-default mode-line-format
+    (list '(:eval (concat (propertize 
+                           (concat " " (format-mode-line mode-name) " ") 
+                           'face 'mode-line-color-1)
+                          (propertize " " 'display arrow-right-1)))
+          '(:eval (concat (propertize " %* %b " 'face 'mode-line-color-2)
+                          (propertize " " 'display arrow-right-2)))
 
-        ;; Justify right by filling with spaces to right fringe - 16
-        ;; (16 should be computed rahter than hardcoded)
-        '(:eval (propertize " " 
-                            'display 
-                            '((space :align-to (- right-fringe 17)))))
+          ;; Justify right by filling with spaces to right fringe - 16
+          ;; (16 should be computed rahter than hardcoded)
+          '(:eval (propertize " " 
+                              'display 
+                              '((space :align-to (- right-fringe 17)))))
 
-        '(:eval (concat (propertize " " 'display arrow-left-2)
-                        (propertize " %p " 'face 'mode-line-color-2)))
-        '(:eval (concat (propertize " " 'display arrow-left-1)
-                        (propertize "%4l:%2c  " 'face 'mode-line-color-3)))
-))
+          '(:eval (concat (propertize " " 'display arrow-left-2)
+                          (propertize " %p " 'face 'mode-line-color-2)))
+          '(:eval (concat (propertize " " 'display arrow-left-1)
+                          (propertize "%4l:%2c  " 'face 'mode-line-color-3)))
+          ))
 
-(make-face 'mode-line-color-1)
-(set-face-attribute 'mode-line-color-1 nil
-                    :foreground color1
-                    :background fontcolor1)
+  (make-face 'mode-line-color-1)
+  (set-face-attribute 'mode-line-color-1 nil
+                      :foreground color1
+                      :background fontcolor1)
 
-(make-face 'mode-line-color-2)
-(set-face-attribute 'mode-line-color-2 nil
-                    :foreground color2
-                    :background fontcolor2)
+  (make-face 'mode-line-color-2)
+  (set-face-attribute 'mode-line-color-2 nil
+                      :foreground color2
+                      :background fontcolor2)
 
-(make-face 'mode-line-color-3)
-(set-face-attribute 'mode-line-color-3 nil
-                    :foreground color3
-                    :background fontcolor1)
+  (make-face 'mode-line-color-3)
+  (set-face-attribute 'mode-line-color-3 nil
+                      :foreground color3
+                      :background fontcolor1)
 
-(set-face-attribute 'mode-line nil
-                    :foreground color3
-                    :background "#000"
-                    :box nil)
-(set-face-attribute 'mode-line-inactive nil
-                    :foreground "#002B35"
-                    :background "#000")
+  (set-face-attribute 'mode-line nil
+                      :foreground color3
+                      :background "#000"
+                      :box nil)
+  (set-face-attribute 'mode-line-inactive nil
+                      :foreground "#002B35"
+                      :background "#000")
 
-))
+  )
+)
