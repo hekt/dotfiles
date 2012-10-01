332c905d2bd2540759a162b0958f5acbde72ee65
hekt <hektorg@gmail.com>
2012-10-02 01:18:33 +0900

    .emacs.d: Add ansi-term settings

--- .emacs.d/inits/10_init.keybind.el
+++ .emacs.d/inits/10_init.keybind.el
@@ -3,21 +3,29 @@
 (setq mac-option-modifier 'meta)
 (define-key global-map (kbd "C-h") 'delete-backward-char)
 (define-key global-map (kbd "C-o") 'other-window)
+(define-key term-raw-map (kbd "C-h") 'delete-backward-char)
+(define-key term-raw-map (kbd "C-o") 'other-window)
 
 ;; sence-region
 (define-key global-map (kbd "C-c s") 'cua-set-rectangle-mark)
 
 ;; anything
 (define-key global-map (kbd "C-c a") 'anything)
+(define-key term-raw-map (kbd "C-c a") 'anything)
 
 ;; magit
 (define-key global-map (kbd "C-c m") 'magit-status)
+(define-key term-raw-map (kbd "C-c m") 'magit-status)
 
 ;; windmove
 (define-key global-map (kbd "C-c p") 'windmove-up)
 (define-key global-map (kbd "C-c n") 'windmove-down) 
 (define-key global-map (kbd "C-c b") 'windmove-left) 
 (define-key global-map (kbd "C-c f") 'windmove-right)
+(define-key term-raw-map (kbd "C-c p") 'windmove-up)
+(define-key term-raw-map (kbd "C-c n") 'windmove-down)
+(define-key term-raw-map (kbd "C-c b") 'windmove-left)
+(define-key term-raw-map (kbd "C-c f") 'windmove-right)
 
 ;; copy / paste / cut
 (if window-system 
@@ -32,14 +40,20 @@
         (message "Copied selection to pasteboard"))
       (defun cut-to-pasteboard (p1 p2) (interactive "r*")
         (ns-set-pasteboard (filter-buffer-substring p1 p2 t)))
-      (global-set-key (kbd "s-v") 'paste-from-pasteboard)
-      (global-set-key (kbd "s-c") 'copy-to-pasteboard)
-      (global-set-key (kbd "s-x") 'cut-to-pasteboard)
+      (define-key global-map (kbd "s-v") 'paste-from-pasteboard)
+      (define-key global-map (kbd "s-c") 'copy-to-pasteboard)
+      (define-key global-map (kbd "s-x") 'cut-to-pasteboard)
+      (define-key term-raw-map (kbd "s-v") 'paste-from-pasteboard)
+      (define-key term-raw-map (kbd "s-c") 'copy-to-pasteboard)
+      (define-key term-raw-map (kbd "s-x") 'cut-to-pasteboard)
       )
   (progn
     (require 'osx-clipboard)
     (define-key global-map (kbd "C-c M-v") 'osx-pbpaste)
     (define-key global-map (kbd "C-c M-c") 'osx-pbcopy)
     (define-key global-map (kbd "C-c M-x") 'osx-pbcut)
+    (define-key term-raw-map (kbd "C-c M-v") 'osx-pbpaste)
+    (define-key term-raw-map (kbd "C-c M-c") 'osx-pbcopy)
+    (define-key term-raw-map (kbd "C-c M-x") 'osx-pbcut)
     )
 )
--- .emacs.d/inits/30_init.appearance.el
+++ .emacs.d/inits/30_init.appearance.el
@@ -4,15 +4,30 @@
 
 ;; set shell-mode colors
 (when window-system
-    (progn
-      (setq ansi-color-names-vector
-            ["#002B36" ; black
-             "#DC322F" ; red
-             "#859900" ; green
-             "#B58900" ; yellow
-             "#268BD2" ; blue
-             "#D33682" ; magenta
-             "#2AA198" ; cyan
-             "#FDF6E3" ; white
-             ])
-      (setq ansi-color-map (ansi-color-make-color-map))))
+  (progn
+    (setq ansi-color-names-vector
+          ["#002B36" ; black
+           "#DC322F" ; red
+           "#859900" ; green
+           "#B58900" ; yellow
+           "#268BD2" ; blue
+           "#D33682" ; magenta
+           "#2AA198" ; cyan
+           "#FDF6E3" ; white
+           ])
+    (setq ansi-color-map (ansi-color-make-color-map))))
+
+;; set ansi-term-mode colors
+(when window-system
+  (progn
+    (setq ansi-term-color-vector
+          [unspecified
+           "#002B36" ; black
+           "#DC322F" ; red
+           "#859900" ; green
+           "#B58900" ; yellow
+           "#268BD2" ; blue
+           "#D33682" ; magenta
+           "#2AA198" ; cyan
+           "#FDF6E3" ; white
+           ])))
