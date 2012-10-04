542faa41571be15ee33005661fa376ee32f195c8
hekt <hektorg@gmail.com>
2012-10-05 04:18:25 +0900

    .emacs.d: Add function add-css-vendor-prefixes

--- .emacs.d/inits/70_init.function.el
+++ .emacs.d/inits/70_init.function.el
@@ -10,3 +10,15 @@
 </code></pre>"
    "<pre class=\"brush: \\1\">\\2</pre>" 
    nil start end))
+
+;; add css vendor prefixes
+(defun add-css-vendor-prefixes (start end)
+  (interactive "r")
+  (replace-regexp
+   "\\( *\\)\\(.*\\)"
+   "\\1-webkit-\\2
+\\1-moz-\\2
+\\1-ms-\\2
+\\1-o-\\2
+\\1\\2"
+   nil start end))
