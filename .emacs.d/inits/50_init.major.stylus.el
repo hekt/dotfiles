53ed6596cffa1a3cebd9c9f7c82ec35fd194d0f6
hekt <hektorg@gmail.com>
2013-06-21 15:25:36 +0900

    .emacs.d: fix stylus-mode after-save-hook

--- .emacs.d/inits/50_init.major.stylus.el
+++ .emacs.d/inits/50_init.major.stylus.el
@@ -4,13 +4,12 @@
 (add-to-list 'auto-mode-alist '("\\.styl$" . stylus-mode))
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
 
-(defun stylus-hook-func ()
-  (add-hook 'after-save-hook 'stylus-after-save-hook-func))
-
 (defun stylus-after-save-hook-func ()
   (shell-command
    (format
     "stylus %s"
     (buffer-file-name (current-buffer)))))
 
-(add-hook 'stylus-mode-hook 'stylus-hook-func)
+(add-hook 'stylus-mode-hook
+  (lambda ()
+    (add-hook 'after-save-hook 'stylus-after-save-hook-func nil t)))
