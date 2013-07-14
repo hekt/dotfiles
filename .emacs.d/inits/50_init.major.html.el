ce84250dcac5f296e629f79be87bd3436c2e2137
hekt <hektorg@gmail.com>
2013-07-14 21:45:36 +0900

    .emacs.d: add html-mode keywords

--- .emacs.d/inits/50_init.major.html.el
+++ .emacs.d/inits/50_init.major.html.el
@@ -1,3 +1,10 @@
+(font-lock-add-keywords
+ 'html-mode
+ '(("%=" . font-lock-keyword-face)
+   ("%" . font-lock-keyword-face)
+   ("?php" . font-lock-keyword-face)
+   ("?" . font-lock-keyword-face)))
+
 (add-hook 'html-mode-hook
           (lambda ()
             (define-key html-mode-map (kbd "<") 'skeleton-pair-insert-maybe)))
