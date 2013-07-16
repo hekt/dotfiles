cb5ecf4055b0b26c761f66c584846cbf7fb83c0a
hekt <hektorg@gmail.com>
2013-07-17 01:18:03 +0900

    .emacs.d: remove char-code settings, use column-number-mode

--- .emacs.d/inits/00_init.basic.el
+++ .emacs.d/inits/00_init.basic.el
@@ -2,19 +2,14 @@
 ;; Basic
 ;;
 
-;; coding
-(set-language-environment 'utf-8)
-(prefer-coding-system 'utf-8)
-(setq file-name-coding-system 'utf-8)
-(setq locale-coding-system 'utf-8)
-
 ;; user info
 (setq user-full-name "hekt")
 (setq user-mail-address "hektorg@gmail.com")
 
 ;; display
 (menu-bar-mode -1)
-(global-hl-line-mode 1)
+(global-hl-line-mode t)
+(column-number-mode t)
 (setq inhibit-splash-screen t)
 (setq initial-buffer-choice t)
 (setq initial-scratch-message nil)
