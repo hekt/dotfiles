41ed98cbb0935d44d4104623950bcd149a32549f
hekt <hektorg@gmail.com>
2013-07-19 00:56:33 +0900

    .emacs.d: refactoring

--- .emacs.d/bin/compile_scss_with_vendor_prefixes.py
+++ .emacs.d/bin/compile_scss_with_vendor_prefixes.py
@@ -5,13 +5,14 @@ import sys
 import re
 from subprocess import Popen, PIPE, STDOUT
 
-PROPERTIES = ['box-sizing', 'box-flex', 'transform']
-PREFIXES = ['-webkit-', '-moz-', '-ms-', '-o-', '']
+PROPERTIES = ["box-sizing", "box-flex", "transform"]
+PREFIXES = ["-webkit-", "-moz-", "-ms-", "-o-", ""]
 
 def addPrefixes(s, prop):
-    r = re.compile("(?:^|[\s\t\n]){0}[\\s\\t]*(:[\\s\\t]*.*?;)".format(prop))
+    r_base = r"(?:^|[\s\t\n;]){0}[\s\S]*?(:[\s\S]*?;)"
+    r = re.compile(r_base.format(prop), re.M)
     def repl(m):
-        return "\n".join([p + prop + m.group(1) for p in PREFIXES])
+        return "".join([p + prop + m.group(1) for p in PREFIXES])
     return re.sub(r, repl, s);
         
 def addPrefixesAll(s):
@@ -22,14 +23,18 @@ def addPrefixesAll(s):
 def compileScss(outputName, outputContent):
     ps = Popen(("sass", "--scss", "--stdin", outputName),
                stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+    
     ps.stdin.write(outputContent)
     ps.stdin.close()
+    
     code =  ps.wait()
     output = ps.stdout.read()
+    
     return (code, output)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     inputContent = sys.stdin.read();
+    
     outputName = sys.argv[1]
     outputContent = addPrefixesAll(inputContent)
 
