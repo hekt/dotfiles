b4dee4f70093f256ec2157f7c8acd899f82b4f5e
hekt <hektorg@gmail.com>
2013-07-20 00:57:28 +0900

    .emacs.d: refactoring, add replaceRem function

--- .emacs.d/bin/compile_scss_with_vendor_prefixes.py
+++ .emacs.d/bin/compile_scss_with_vendor_prefixes.py
@@ -5,38 +5,67 @@ import sys
 import re
 from subprocess import Popen, PIPE, STDOUT
 
+
 PROPERTIES = ["box-sizing", "box-flex", "transform"]
 PREFIXES = ["-webkit-", "-moz-", "-ms-", "-o-", ""]
 
+
 def addPrefixes(s, prop):
-    r_base = r"(?:^|[\s\t\n;]){0}[\s\S]*?(:[\s\S]*?;)"
-    r = re.compile(r_base.format(prop), re.M)
-    def repl(m):
-        return "".join([p + prop + m.group(1) for p in PREFIXES])
-    return re.sub(r, repl, s);
-        
-def addPrefixesAll(s):
+    reg = re.compile(r"(?:^|[\s\t\n\r;]){0}[\s\S]*?(:[\s\S]*?;)".format(prop),
+                     re.M)
+    repl = lambda m: "".join([p + prop + m.group(1) for p in PREFIXES])
+
+    return re.sub(reg, repl, s)
+
+
+def replaceRem(s):
+    """Add `font-size` and `line-height` after `font` that uses `rem`.
+
+    A font shorthand property that uses rem in IE is not work. This
+    function replaces font-size and line-height definition in the font
+    property by `em` and add a font-size and a line-height property that
+    define actual value.
+    """
+
+    fs_reg = re.compile(r"(font[\s\t\n\r]*?:.*?)(?<=[^/])([0-9\.]+)rem(.*?;)")
+    fs_repl = lambda m: ("{0}{1}em{2}font-size:{1}rem;"
+                         .format(m.group(1), m.group(2), m.group(3)))
+    lh_reg = re.compile(r"(font[\s\t\n\r]*?:.*?)(?<=/)([0-9\.]+)rem(.*?;)")
+    lh_repl = lambda m: ("{0}{1}em{2}line-height:{1}rem;"
+                         .format(m.group(1), m.group(2), m.group(3)))
+
+    s = re.sub(lh_reg, lh_repl, s)
+    s = re.sub(fs_reg, fs_repl, s)
+
+    return s
+
+
+def doAll(s):
     for p in PROPERTIES:
         s = addPrefixes(s, p)
+    s = replaceRem(s)
+
     return s
+
 
 def compileScss(outputName, outputContent):
     ps = Popen(("sass", "--scss", "--stdin", outputName),
                stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-    
+
     ps.stdin.write(outputContent)
     ps.stdin.close()
-    
-    code =  ps.wait()
+
+    code = ps.wait()
     output = ps.stdout.read()
-    
+
     return (code, output)
 
-if __name__ == '__main__':
-    inputContent = sys.stdin.read();
-    
-    outputName = sys.argv[1]
-    outputContent = addPrefixesAll(inputContent)
 
-    c, o =  compileScss(outputName, outputContent)
+if __name__ == '__main__':
+    inputContent = sys.stdin.read()
+
+    outputName = sys.argv[1]
+    outputContent = doAll(inputContent)
+
+    c, o = compileScss(outputName, outputContent)
     sys.exit(c) if (c == 0) else sys.exit(o)
