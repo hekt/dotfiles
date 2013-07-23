985f3cba15af7a29abbc3bab40c5c8d7733201b2
hekt <hektorg@gmail.com>
2013-07-23 19:25:47 +0900

    .emacs.d: move test script

--- .emacs.d/bin/tests/scss_preprocessor_test.py
+++ .emacs.d/bin/tests/scss_preprocessor_test.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import sys, os
+dirpath = os.path.dirname(os.path.abspath(__file__))
+parpath = os.path.join(dirpath, os.pardir)
+sys.path.append(parpath)
+
+
 import unittest
 import scss_preprocessor as app
 
