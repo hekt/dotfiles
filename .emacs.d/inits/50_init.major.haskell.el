6e6d934ec3d0c8b2f3def5382500f502d5e2563c
hekt <hektorg@gmail.com>
2013-08-10 17:59:08 +0900

    .eamcs.d: remove commented line, change ghci path

--- .emacs.d/inits/50_init.major.haskell.el
+++ .emacs.d/inits/50_init.major.haskell.el
@@ -1,8 +1,8 @@
 ;; haskell
 
-;; (load "~/.emacs.d/site-lisp/haskell-mode/haskell-site-file")
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
-(setq haskell-program-name "/usr/bin/ghci")
+(setq haskell-program-name "/usr/local/bin/ghci")
+
