76276c4ab891d038e60364991dd695d1976c288d
hekt <hektorg@gmail.com>
2013-12-15 22:00:00 +0900

    .emacs.d: modify linum-mode settings

--- .emacs.d/inits/30_init.appearance.el
+++ .emacs.d/inits/30_init.appearance.el
@@ -3,6 +3,9 @@
 ;; linum
 (global-linum-mode t)
 (setq linum-format "%3d ")
+(setq linum-delay t)
+(defadvice linum-schedule (around my-linum-schedule () activate)
+  (run-with-idle-timer 0.2 nil #'linum-update-current))
 
 ;; modeline
 (setq-default
