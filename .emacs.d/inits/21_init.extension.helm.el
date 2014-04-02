b45dd81d4cc7876397d34da7e11bfe905fa738ee
hekt <hektorg@gmail.com>
2014-04-03 00:41:16 +0900

    .emacs.d: update helm settings

--- .emacs.d/inits/21_init.extension.helm.el
+++ .emacs.d/inits/21_init.extension.helm.el
@@ -1,22 +1,24 @@
 ;; Helm
 
-(custom-set-variables
- '(helm-truncate-lines t))
+(when (require 'helm-config nil t)
+  (helm-mode 0)
+  
+  (custom-set-variables
+   '(helm-truncate-lines t))
 
-;; keybinds
+  ;; keybinds
+  (define-key global-map (kbd "C-c a") 'helm-mini)
+  (define-key global-map (kbd "C-c C-f") 'helm-find-files)
+  (define-key global-map (kbd "M-y") 'helm-show-kill-ring)
 
-(define-key global-map (kbd "C-c a") 'helm-mini)
-(define-key global-map (kbd "C-c C-f") 'helm-find-files)
-;; (define-key global-map (kbd "C-c C-r") 'helm-recentf)
-(define-key global-map (kbd "M-y") 'helm-show-kill-ring)
+  ;; enable C-h in helm
+  (define-key helm-map (kbd "C-h") 'delete-backward-char)
+  (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
 
-;; enable C-h in helm
-(define-key helm-map (kbd "C-h") 'delete-backward-char)
-(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
-
-;; Emulate 'kill-line' in helm minibuffer
-(setq helm-delete-minibuffer-contents-from-point t)
-(defadvice helm-delete-minibuffer-contents 
-  (before helm-emulate-kill-line activate)
-  "Emulate 'kill-line' in helm minibuffer"
-  (kill-new (buffer-substring (point) (field-end))))
+  ;; Emulate 'kill-line' in helm minibuffer
+  (setq helm-delete-minibuffer-contents-from-point t)
+  (defadvice helm-delete-minibuffer-contents 
+    (before helm-emulate-kill-line activate)
+    "Emulate 'kill-line' in helm minibuffer"
+    (kill-new (buffer-substring (point) (field-end))))
+  )
