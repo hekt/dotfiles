44faee2509655dd0e78ec50786827ab8ddb9970e
hekt <hektorg@gmail.com>
2015-03-22 22:02:21 +0900

    Remove input-status from tmux statusbar

--- .tmux/input.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-ISRC=`~/bin/inputsource | sed 's/.*\.//g'`
-if [ $ISRC = 'Roman' ]; then
-    echo "#[fg=white,bg=black] A #[default]"
-else
-    echo "#[fg=white,bg=red] J #[default]"
-fi
