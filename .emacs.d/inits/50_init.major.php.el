c49a376e37c625e2948c9dba77cf111276f30b59
Kazutoshi Horie <k-horie@k-horie.local>
2015-05-14 10:06:23 +0900

    Change indent setting

--- .emacs.d/inits/50_init.major.php.el
+++ .emacs.d/inits/50_init.major.php.el
@@ -5,5 +5,7 @@
 (defun php-mode-hooks ()
   (define-key php-mode-map (kbd "(") 'skeleton-pair-insert-maybe)
   (define-key php-mode-map (kbd "{") 'skeleton-pair-insert-maybe)
-  (setq skeleton-pair 1))
-(add-hook 'php-mode-hook 'php-mode-hooks)
+  (setq skeleton-pair 1)
+  (setq tab-width 4
+        c-basic-offset 4
+        indent-tabs-mode t))
