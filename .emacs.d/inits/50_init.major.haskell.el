1edc2c3492c48cf515f93bf3d57fada7187666ad
hekt <hektorg@gmail.com>
2015-06-13 16:37:10 +0900

    Improve haskell settings

--- .emacs.d/inits/50_init.major.haskell.el
+++ .emacs.d/inits/50_init.major.haskell.el
@@ -1,7 +1,7 @@
 ;; haskell
 
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (setq haskell-program-name "/usr/local/bin/ghci")
