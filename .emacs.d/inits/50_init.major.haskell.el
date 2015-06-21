cd565d712ece8b8f54d853d3c1f77b43405d62d9
hekt <hektorg@gmail.com>
2015-06-21 20:00:32 +0900

    Change haskell's indent mode

--- .emacs.d/inits/50_init.major.haskell.el
+++ .emacs.d/inits/50_init.major.haskell.el
@@ -2,6 +2,6 @@
 
 ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
-;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (setq haskell-program-name "/usr/local/bin/ghci")
