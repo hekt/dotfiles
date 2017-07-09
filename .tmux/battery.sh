b8e72e1eb8b00f3fcb3b49f82ecb5f7f7ae2c514
hekt <hektorg@gmail.com>
2017-07-09 10:14:47 +0900

    update tmux configs

--- .tmux/battery.sh
+++ .tmux/battery.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-cg=`pmset -g ps | awk '{ if (NR == 2) print $3 }'`
+cg=`pmset -g ps | awk '{ if (NR == 2) print $4 }'`
 if [ $cg = "charged;" ]; then
     echo "(~)"
 elif [ $cg = "charging;" ] || [ $cg = "finishing" ]; then
     echo ")~}"
 else
-    bt=`pmset -g ps | awk '{ if (NR == 2) print $2 }' | sed 's/%;//g'`
+    bt=`pmset -g ps | awk '{ if (NR == 2) print $3 }' | sed 's/%;//g'`
     if [ $((bt)) -gt 50 ]; then
         printf ")#[default]%02d#[default]}" $bt
     elif [ $((bt)) -gt 25 ]; then
