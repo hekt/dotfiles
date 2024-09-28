d1619d1e57d6a807c6cc5fda49072b8404050152
hekt <hektorg@gmail.com>
2024-09-28 23:22:31 +0900

    update wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,13 +2,27 @@ local wezterm = require "wezterm"
 local act = wezterm.action
 local config = wezterm.config_builder()
 
+-- see: https://wezfurlong.org/wezterm/config/lua/config/term.html
+config.term = "wezterm"
 config.color_scheme = "Tomorrow Night Eighties"
 config.font = wezterm.font "Hackgen Console NF"
 config.font_size = 14.0
-line_height = 1.2
+config.line_height = 1.2
+config.hide_tab_bar_if_only_one_tab = true
+config.default_cwd = wezterm.homedir
 config.scrollback_lines = 3500
 config.enable_scroll_bar = false
 config.window_background_opacity = 0.9
+config.macos_window_background_blur = 10
+config.native_macos_fullscreen_mode = true
+config.swallow_mouse_click_on_pane_focus = true
+config.swallow_mouse_click_on_window_focus = true
+config.window_padding = {
+	left = "1cell",
+	right = "1cell",
+	top = "0.5cell",
+	bottom = "0.5cell",
+}
 config.mouse_bindings = {
     -- Change the default click behavior so that it only selects
     -- text and doesn't open hyperlinks
