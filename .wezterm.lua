67c0373840a93717a18f6e1e392417bff0cd2441
hekt <hektorg@gmail.com>
2024-09-28 10:59:35 +0900

    disable open link on click

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
 local wezterm = require "wezterm"
-
+local act = wezterm.action
 local config = wezterm.config_builder()
 
 config.color_scheme = "Tomorrow Night Eighties"
@@ -9,5 +9,20 @@ line_height = 1.2
 config.scrollback_lines = 3500
 config.enable_scroll_bar = false
 config.window_background_opacity = 0.9
+config.mouse_bindings = {
+    -- Change the default click behavior so that it only selects
+    -- text and doesn't open hyperlinks
+    {
+        event = { Up = { streak = 1, button = "Left" } },
+        mods = "NONE",
+        action = act.CompleteSelection 'ClipboardAndPrimarySelection',
+    },
+    -- and make Cmd-Click open hyperlinks
+    {
+        event = { Up = {streak = 1, button = "Left" } },
+        mods = "CMD",
+        action = "OpenLinkAtMouseCursor",
+    },
+}
 
 return config
